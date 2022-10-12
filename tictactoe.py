c883af2b724a50eb7d5f544d3633f5488f0067d2
Xero0a <chipsikchannel@gmail.com>
2022-10-12 11:18:18 +0300

    Переписал функцию определения победителя, переименовал некоторые функции и переменные

--- tictactoe.py
+++ tictactoe.py
@@ -8,20 +8,22 @@ class TicTacToe(Canvas):
 
         self.window = window
         super().__init__(window, width=300, height=300, bg="seashell2")
-        self.state = {(0,0):None, (1,0):None, (2,0):None, (0,1):None, (1,1):None, (2,1):None, (0,2):None, (1,2):None, (2,2):None}
+        self.state = {(0,0):None, (1,0):None, (2,0):None, (0,1):None, (1,1):None,
+                      (2,1):None, (0,2):None, (1,2):None, (2,2):None}
         self.bind("<Button-1>", self.click)
 
     def get_winner(self):
-        """Определяем победителя по комбинации символов(o или x)."""
+        """Получаем победителя по комбинации символов(o или x) и выводим его на экран."""
 
-        winner_combo = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
-                        (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
-
-        list_cells = [val for val in self.state.values()]
-        if ['x', 'x', 'x'] in winner_combo:
+        cells = [val for val in self.state.values()]
+        win_combo = [
+            cells[0:3], cells[3:6], cells[6:9], cells[0:7:3], cells[1:8:3],
+            cells[2:9:3], cells[0:9:4], cells[2:7:2]
+        ]
+        if ['x', 'x', 'x'] in win_combo:
             winner = "x"
             game.show_winner(winner)
-        elif ['o', 'o', 'o'] in winner_combo:
+        elif ['o', 'o', 'o'] in win_combo:
             winner = "o"
             game.show_winner(winner)
         elif None not in self.state.values():
@@ -30,28 +32,29 @@ class TicTacToe(Canvas):
     
 
     def bot_move(self):
-        """Отрисовка O в ответ на X."""
+        """Отрисовка нолика в ответ на ход игрока."""
 
         if None in self.state.values():   
             while True:
-                random_index = (randint(0, 2), randint(0, 2))
-                if self.state[random_index] is None:
+                random_key = (randint(0, 2), randint(0, 2))
+                if self.state[random_key] is None:
                     break
-            self.state[random_index] = "o"
 
-            self.add_o(random_index)
+            self.draw_o(random_key)
+            self.state[random_key] = "o"
 
 
 
     def click(self, event):
-        """Вычисляет координаты нажатия, вычисляет колонну, строку, индекс клетки по координатам."""
+        """Вычисляет координаты нажатия, вычисляет колонну, строку, ключ клетки по координатам."""
+        
         pos_x = event.x // 100
         pos_y = event.y // 100
-        index = (pos_x, pos_y)
+        key = (pos_x, pos_y)
 
-        if self.state[index] is None:
-            self.state[index] = "x"
-            self.add_x(index)
+        if self.state[key] is None:
+            self.state[key] = "x"
+            self.draw_x(key)
             self.bot_move()
             self.get_winner()
 
@@ -64,28 +67,28 @@ class TicTacToe(Canvas):
             self.create_line(0, 100 * iteration, 300,
                              100 * iteration, fill='Grey')
 
-    def add_x(self, index):
-        """Рисует крестики."""
+    def draw_x(self, key):
+        """Рисует крестик."""
 
-        self.create_line(index[0]*100+10, index[1]*100+10,
-                         index[0]*100+90, index[1]*100+90, width=5)
-        self.create_line(index[0]*100+90, index[1]*100+10,
-                         index[0]*100+10, index[1]*100+90, width=5)
+        self.create_line(key[0]*100+10, key[1]*100+10,
+                         key[0]*100+90, key[1]*100+90, width=5)
+        self.create_line(key[0]*100+90, key[1]*100+10,
+                         key[0]*100+10, key[1]*100+90, width=5)
 
-    def add_o(self, index):
-        """Рисует нолики."""
+    def draw_o(self, key):
+        """Рисует нолик."""
 
         self.create_oval(
-            index[0]*100+10,
-            index[1]*100+10,
-            index[0]*100+100-10,
-            index[1]*100+100-10,
+            key[0]*100+10,
+            key[1]*100+10,
+            key[0]*100+100-10,
+            key[1]*100+100-10,
             width=5,
             outline="navajo white"
         )
 
     def show_winner(self, winner):
-        """Выводит победителя на экран"""
+        """Выводит победителя на экран."""
 
         self.create_rectangle(0, 0, 310, 310, fill='seashell2')
         winner_banner = game.create_text(150, 100, text='', font=("Arial", 20))
